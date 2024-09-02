b9d68a2e2231adbbfefe5b41000aaa5b4b1803e6
Kipkoech78 <lkipkoech@kabarak.ac.ke>
2024-09-02 19:17:56 +0300

    Retrofit Trial

---
+++ app/src/main/java/com/example/utils/MyRadioButton.kt
@@ -0,0 +1,20 @@
+package com.example.utils
+
+import android.content.Context
+import android.graphics.Typeface
+import android.util.AttributeSet
+import androidx.appcompat.widget.AppCompatRadioButton
+
+class RadioButton(context: Context, attributeSet: AttributeSet): AppCompatRadioButton(context, attributeSet) {
+
+    init {
+        //call function to apply font to components
+        applyFont()
+    }
+
+    private fun applyFont() {
+        val typeface: Typeface =
+            Typeface.createFromAsset(context.assets, "sf_pro_display.OTF")
+        setTypeface(typeface)
+    }
+}
